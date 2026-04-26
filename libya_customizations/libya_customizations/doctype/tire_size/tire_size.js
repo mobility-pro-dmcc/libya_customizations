e507476dc46355f67c061bb29afb72c0c6f044ec
Ahmed Sharaf <ahmed.sharafeddin@gmail.com>
2026-04-26 07:02:19 +0000

    Refactor: Item Specs

---
+++ libya_customizations/libya_customizations/doctype/tire_size/tire_size.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Ahmed Zaytoon and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Tire Size", {
+// 	refresh(frm) {
+
+// 	},
+// });
