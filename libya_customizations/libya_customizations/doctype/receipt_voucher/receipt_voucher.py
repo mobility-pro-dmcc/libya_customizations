5e92b3612365bef51a717b0edd9ec774edcecada
Ahmed Zaytoon <citybirdman@gmail.com>
2026-06-09 13:45:35 +0000

    fix: ppr error

--- libya_customizations/libya_customizations/doctype/receipt_voucher/receipt_voucher.py
+++ libya_customizations/libya_customizations/doctype/receipt_voucher/receipt_voucher.py
@@ -3,7 +3,7 @@
 
 import frappe
 from frappe.model.document import Document
-from mobility_customizations import reconcile_payments as reconcile_payments_entries
+from libya_customizations import reconcile_payments as reconcile_payments_entries
 from frappe import _
 
 
