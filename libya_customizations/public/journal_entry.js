5642e430ecdfb6621f938a8f42affac65a158c0e
Ahmed Zaytoon <citybirdman@gmail.com>
2026-07-14 10:09:43 +0000

    only disable cancel for linked journal entry

--- libya_customizations/public/journal_entry.js
+++ libya_customizations/public/journal_entry.js
@@ -1,8 +1,8 @@
 frappe.ui.form.on("Journal Entry", {
   	refresh:function(frm){
       	if(cur_frm.doc.custom_voucher_no){
-            frm.disable_save();
-            frm.page.clear_primary_action();
+            // frm.disable_save();
+            // frm.page.clear_primary_action();
             frm.page.clear_secondary_action();
         }
     }
